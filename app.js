62d1695f153ada925add150ee57f5f0f1f3284a8
Matteo <M.Jakob.Inf21@stud.bbbaden.ch>
2022-12-21 09:44:40 +0100

    Added Calc

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+checkCustom();
+var conversionMethod = document.getElementById("conversion");
+conversionMethod.addEventListener("change", checkCustom);
+
+function checkCustom() {
+  var customNum = document.getElementById("custom-number");
+  var conversionMethod = document.getElementById("conversion");
+  if (conversionMethod.value == "custom") {
+    customNum.style.visibility = "visible";
+  } else {
+    customNum.style.visibility = "hidden";
+  }
+}
+function CalcSubmit() {
+  var number = document.getElementById("number").value;
+  var method = document.getElementById("conversion").value;
+  if (method == "custom") {
+    method = document.getElementById("custom-number").value;
+  }
+  convertFrom10(number, method);
+}
+
 function convertFrom10(input, to) {
   let newNumber = "";
   let rest = input;
